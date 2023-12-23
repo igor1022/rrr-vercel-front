d6431576e0765581ea8a6f332045bfb2691cb9bb
Igor <imitroshichev@gmail.com>
2023-12-23 11:36:32 +0200

    third

--- src/Header.js
+++ src/Header.js
@@ -3,6 +3,19 @@ import { useState, useEffect } from 'react';
 import axios from 'axios';
 import './Css/Header.css';
 import { isAuth as realyIsAuth, login, logout } from './auth';
+import { createTheme } from '@mui/material/styles';
+import { ThemeProvider } from '@mui/material/styles';
+import { Button, Typography } from '@mui/material';
+
+// Создаем тему Material UI
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#004d40', // Основной темно-зеленый цвет
+      darker: '#00372E', // Темнее основного цвета для hover
+    },
+  },
+});
 
 function Header(props) {
   let set_logout = {props}.props.setLoading;
@@ -12,17 +25,29 @@ function Header(props) {
     set_logout(result1.data);
     setIsAuth(result1.data);
   }
-    return (
-        <div className="Header">
-          <h1>SFPOPOS</h1>
-                <div className="Header-Subtitle">San Franciscos Privately Owned Public Spaces</div>
-                <div className='Log'>
-                    <button type="button" onClick={handler}>
-                      Logout
-                    </button>
-                </div>    
+  return (
+    <ThemeProvider theme={theme}>
+      <div className="Header" style={{ backgroundColor: theme.palette.primary.main, color: '#fff' }}>
+        <Typography variant="h1">ЗЕМЛЕХАБ</Typography>
+        <Typography variant="subtitle1">Зростають знання - зростає твоя інвестиція</Typography>
+        <div className='Log'>
+          <Button
+            variant="contained"
+            color="primary"
+            onClick={handler}
+            sx={{
+              backgroundColor: theme.palette.primary.darker,
+              '&:hover': {
+                backgroundColor: theme.palette.primary.main,
+              },
+            }}
+          >
+            Вийти
+          </Button>
         </div>
-      )
+      </div>
+    </ThemeProvider>
+  );
 }
 
 export default Header
